5aedb5c31fdb93028a564cb20bb84e78c85cc169
Jonathan Matthews <jonathan.matthews@digital.cabinet-office.gov.uk>
2018-10-08 07:41:59 +0100

    CloudFoundryTask.Run() should omit those things that can be done at startup

--- core/cloudfoundrytask.go
+++ core/cloudfoundrytask.go
@@ -2,8 +2,8 @@ package core
 
 import (
 	"fmt"
-	cfclient "github.com/cloudfoundry-community/go-cfclient"
-	cfenv "github.com/cloudfoundry-community/go-cfenv"
+	"github.com/cloudfoundry-community/go-cfclient"
+	"github.com/cloudfoundry-community/go-cfenv"
 	"github.com/jpillora/backoff"
 	"net/url"
 	"time"
@@ -11,57 +11,41 @@ import (
 
 type CloudFoundryTask struct {
 	BareJob
-	CloudFoundryCredentialsService string `gcfg:"cf-credentials-service"`
-	CloudFoundryAppName            string `gcfg:"cf-appname"`
+	Client              *cfclient.Client
+	Env                 *cfenv.App
+	CloudFoundryAppName string `gcfg:"cf-appname"`
 }
 
-func NewCloudFoundryTask() *CloudFoundryTask {
-	return &CloudFoundryTask{}
-}
-
-func (j *CloudFoundryTask) Run(ctx *Context) error {
-	if !cfenv.IsRunningOnCF() {
-		return fmt.Errorf("a cftask can only be run from an application running *inside* Cloud Foundry")
+func NewCloudFoundryTask(c *cfclient.Client, a *cfenv.App) *CloudFoundryTask {
+	return &CloudFoundryTask{
+		Client: c,
+		Env:    a,
 	}
+}
+
+func BuildCloudFoundryContext() (*cfclient.Client, *cfenv.App, error) {
 	myEnv, err := cfenv.Current()
 	if err != nil {
-		return err
+		return nil, nil, err
 	}
-
-	credentialsService, err := myEnv.Services.WithName(j.CloudFoundryCredentialsService)
+	login, err := myEnv.Services.WithName("scheduler-cf-login")
 	if err != nil {
-		return err
+		return nil, nil, fmt.Errorf("'scheduler-cf-login' service not found")
 	}
-
 	c := &cfclient.Config{
 		ApiAddress: myEnv.CFAPI,
-		Username:   credentialsService.Credentials["username"].(string),
-		Password:   credentialsService.Credentials["password"].(string),
+		Username:   login.Credentials["username"].(string),
+		Password:   login.Credentials["password"].(string),
 	}
 	client, err := cfclient.NewClient(c)
 	if err != nil {
-		return err
+		return nil, nil, err
 	}
+	return client, myEnv, nil
+}
 
-	v := url.Values{}
-	v.Set("q", fmt.Sprintf("name:%s", j.CloudFoundryAppName))
-	v.Add("q", fmt.Sprintf("space_guid:%s", myEnv.SpaceID))
-	app, err := client.ListAppsByQuery(v)
-	if err != nil {
-		return err
-	}
-	switch x := len(app); {
-	case x == 1: // all is good
-	case x == 0: // app not found
-		return fmt.Errorf("Cloud Foundry app '%s' not present in space '%s'", j.CloudFoundryAppName, myEnv.SpaceName)
-	case x > 1: // something odd happened - maybe our ListAppsByQuery() *wasn't* space-scoped?
-		return fmt.Errorf("More than one Cloud Foundry app '%s' present in space '%s'", j.CloudFoundryAppName, myEnv.SpaceName)
-	}
-	task, err := client.CreateTask(cfclient.TaskRequest{
-		Name:        j.Name,
-		Command:     j.Command,
-		DropletGUID: app[0].Guid,
-	})
+func (cft *CloudFoundryTask) Run(ctx *Context) error {
+	task, err := cft.createTask()
 	if err != nil {
 		return err
 	}
@@ -75,7 +59,7 @@ func (j *CloudFoundryTask) Run(ctx *Context) error {
 
 	for {
 		time.Sleep(delay.Duration())
-		task, err = client.GetTaskByGuid(task.GUID)
+		task, err = cft.Client.GetTaskByGuid(task.GUID)
 		if err != nil {
 			return err
 		}
@@ -90,4 +74,35 @@ func (j *CloudFoundryTask) Run(ctx *Context) error {
 		}
 	}
 	return fmt.Errorf("not really sure how we got here ...")
+}
+
+func (cft *CloudFoundryTask) appGuid() (string, error) {
+	v := url.Values{}
+	v.Set("q", fmt.Sprintf("name:%s", cft.CloudFoundryAppName))
+	v.Add("q", fmt.Sprintf("space_guid:%s", cft.Env.SpaceID))
+	app, err := cft.Client.ListAppsByQuery(v)
+	if err != nil {
+		return "", err
+	}
+	switch x := len(app); {
+	case x == 1: // all is good - log something?
+	case x == 0: // app not found
+		return "", fmt.Errorf("app '%s' not found in space '%s'", cft.CloudFoundryAppName, cft.Env.SpaceName)
+	case x > 1: // something odd happened - maybe our ListAppsByQuery() *wasn't* space-scoped?
+		return "", fmt.Errorf("app '%s' not unique in space '%s'", cft.CloudFoundryAppName, cft.Env.SpaceName)
+	}
+	return app[0].Guid, nil
+}
+
+func (cft *CloudFoundryTask) createTask() (task cfclient.Task, err error) {
+	appGuid, err := cft.appGuid()
+	if err != nil {
+		return task, err
+	}
+	task, err = cft.Client.CreateTask(cfclient.TaskRequest{
+		Name:        cft.Name,
+		Command:     cft.Command,
+		DropletGUID: appGuid,
+	})
+	return task, err
 }
